910f2a7cb80cf4d3f625611928f73673fd96f635
adavidliang <adavidliang@gmail.com>
2023-06-10 15:19:41 -0400

    search

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,20 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+    if (word.length === 0) {
+        return false;
     }
+    for (let row of letters) {
+        const rowOfLetter = row.join('');
+        if (rowOfLetter.includes(word)) {
+            return true;
+        }
+    }
+    return false;
+
+    // const horizontalJoin = letters.map(ls => ls.join(''))
+
+    // for (l of horizontalJoin) {
+    //     if (l.includes(word)) return true
+    // }
 }
 
 module.exports = wordSearch
